d62e1d956ecbdcfb642d96876871f4c6fd6a552b
unknown <ami145388@gmail.com>
2025-01-27 15:16:42 +0500

    init

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Toaster } from 'react-hot-toast';
+
+import { SITE_NAME } from '@/constants/seo.constants';
+
+import './globals.css';
+import { Providers } from './providers';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: {
+    default: `${SITE_NAME}`,
+    template: `%s | ${SITE_NAME}`,
+  },
+  description: 'Pass your exams on 100%',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          {children}
+          <Toaster position="top-center" toastOptions={{ duration: 1500 }} />
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ client/src/types/api.types.ts
@@ -0,0 +1,18 @@
+export enum EAuthMethod {
+  LOGIN = 'login',
+  REGISTER = 'register',
+}
+
+export enum EStorageKey {
+  ACCESS_TOKEN = 'accessToken',
+  REFRESH_TOKEN = 'refreshToken',
+  // USER = 'user'
+}
+
+export enum ERequestMethod {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  PATCH = 'PATCH',
+  DELETE = 'DELETE',
+}
